bbe502b1ed28a0a4b7becce6b8d64ff8f913d624
unknown <738506508@qq.com>
2020-12-20 23:24:29 +0800

    util update

--- web/static/util.js
+++ web/static/util.js
@@ -2,6 +2,24 @@ window.util = (()=>{
   const realType = (val)=>{
     return Object.prototype.toString.call(val)
   }
+  Date.prototype.format = function(fmt)   
+  { //author: meizz   
+    var o = {   
+      "M+" : this.getMonth()+1,                 //月份   
+      "d+" : this.getDate(),                    //日   
+      "h+" : this.getHours(),                   //小时   
+      "m+" : this.getMinutes(),                 //分   
+      "s+" : this.getSeconds(),                 //秒   
+      "q+" : Math.floor((this.getMonth()+3)/3), //季度   
+      "S"  : this.getMilliseconds()             //毫秒   
+    };   
+    if(/(y+)/.test(fmt))   
+      fmt=fmt.replace(RegExp.$1, (this.getFullYear()+"").substr(4 - RegExp.$1.length));   
+    for(var k in o)   
+      if(new RegExp("("+ k +")").test(fmt))   
+    fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substr((""+ o[k]).length)));   
+    return fmt;   
+  }    
   let toastList = []
   return {
     createDom:(str)=>{
@@ -73,6 +91,11 @@ window.util = (()=>{
       },5000)
       toastList.push(node)
       document.body.appendChild(node)
+    },
+    clearTimeout:(tm)=>{
+      tm.forEach(item=>{
+        clearTimeout(item)
+      })
     }
   }
 })()
