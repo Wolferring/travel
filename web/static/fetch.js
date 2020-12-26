5011928fd02dbb4b0b04154c91a4fba8e1d62489
unknown <738506508@qq.com>
2020-12-26 10:50:07 +0800

    1

--- web/static/fetch.js
+++ web/static/fetch.js
@@ -1,6 +1,6 @@
 window.api = (()=>{
 
-  let apiurl = "//localhost:3000";
+  let apiurl = "//192.168.0.117:3000";
   if( (location.host).indexOf("travel")!=-1){
       apiurl ="//travel.whimsylove.cn/api";
   }
